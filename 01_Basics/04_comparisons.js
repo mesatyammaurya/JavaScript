dba17cbf82427800fdf5d05da861eb17c0e32d6c
mesatyammaurya <mauryasatyam355@gmail.com>
2025-03-03 03:20:39 +0530

    conversions

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>=1);
+
+// console.log("2">1);
+// console.log("02" >1);
+// always use same data types
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+console.log(null<=0); //true
+console.log(null<0); //false
+// since comparison converts null to zero hence it becomes true in case of >= or <=
+console.log(undefined<=0);
+console.log(undefined >=0);
+// this always gives false
+// avoid all these conversion 
+
+// # Strict Check - (===)
+console.log("2"===2); // false
+
+
